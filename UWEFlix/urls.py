61e71f0f44149a9fdfb3616187d9c2f6267ab59a
Luca <luca.pike7@gmail.com>
2023-02-04 21:05:24 +0000

    login page added, reiredct not working

--- UWEFlix/urls.py
+++ UWEFlix/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("home", views.home, name="home"),
+    path("login", views.login_user, name="login")
 ]
 
--- UWEFlix/views.py
+++ UWEFlix/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return HttpResponse('Hello Django')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("there was an error logging in, please try again"))
+            return redirect('login')
+    else:
+        return render(request, 'UWEFlix/login.html', {})

92ef4852b6c44e872ef7477eabafc3af5197c774
Luca <luca.pike7@gmail.com>
2023-02-20 16:28:25 +0000

    club, contact and address models added

--- UWEFlix/models.py
+++ UWEFlix/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.password_validation import validate_password
+# from django.contrib.auth.password_validation import validate_password
 
-# class CustomUserCreationForm(UserCreationForm):
-#     password1 = forms.CharField(
-#         label = "Password",
-#         strip = False,
-#         widget = forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-#         help_text = validate_password.password_validators_help_text_html(),
-#     )
-#     password2 = forms.CharField(
-#         label = "Password confirmation",
-#         strip = False,
-#         widget = forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-#         help_text = "Enter the same password as before, for verification",
-#     )
+class Address(models.Model): 
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=255)
 
-#     class Meta:
-#         model = User
-#         fields 
+    def __str__(self) -> str:
+        return self.street_number + ' ' + self.street
+
+
+class ContactDetails(models.Model):
+    mobile_number = models.CharField(max_length=50)
+    landline_number = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        return self.email
+
+
+class Club(models.Model):
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
+    contact_details = models.ForeignKey(ContactDetails, on_delete=models.CASCADE, null=True)
+    club_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.club_name
